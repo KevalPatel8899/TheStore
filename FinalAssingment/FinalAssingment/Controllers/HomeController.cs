aab5eba49759dd8ab8fbbae71e4421514d73b914
Keval Patel <200393482@student,georgianc.on.ca>
2019-03-20 12:02:31 -0400

    InformationChanges

---
+++ FinalAssingment/FinalAssingment/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalAssingment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This page is for site description.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Group 9 contect Page.";
+
+            return View();
+        }
+    }
+}
---
+++ FinalAssingment/FinalAssingment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinalAssingment.Startup))]
+namespace FinalAssingment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
