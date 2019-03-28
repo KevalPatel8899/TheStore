3f795e6571ee2a4b0a005f1dbe0a3ea8054fb538
Keval Patel <200393482@student,georgianc.on.ca>
2019-03-27 22:55:56 -0400

    Products & Category

--- FinalAssingment/FinalAssingment/Models/Product.cs
+++ FinalAssingment/FinalAssingment/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,29 @@ namespace FinalAssingment.Models
 {
     public class Product
     {
-        public String Name { get; set; }
+        //Primery Key
+        [Required]
+        public int ProductId { get; set; }
+
+        //Product Name
+        [Required]
+        public string Name { get; set; }
+
+        //Product Description
+        [Required]
+        public string Description { get; set; }
+
+        //Product Price in decimals
+        [Required]
+        [Range (1,10000000)]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public decimal Price { get; set; }
+
+        //Product Categoty ID as foreign key
+        public int CategoryId { get; set; }
+
+        //Product category NAME
+        public virtual Category CategoryName { get; set; }
+
     }
 }
